c96d0c42a40d29323dfb85509b124ad21b496927
Nicolas Pedernera <nicopedernera12@gmail.com>
2022-10-28 18:35:17 -0300

    cors**

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
   app.setGlobalPrefix('api/v2');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,12 +15,7 @@ async function bootstrap() {
       },
     }),
   );
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  
 
   await app.listen(process.env.PORT);
   console.log(`App running on port: "${process.env.PORT}"...`);
